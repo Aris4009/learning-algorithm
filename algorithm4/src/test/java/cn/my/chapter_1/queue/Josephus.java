ea39dfeb1d3d5ab34e9c9433d8d0261d8a027df0
aris <slut@vip.qq.com>
2021-10-27 14:29:23 +0800

    使用迭代器实现约瑟夫环

--- algorithm4/src/test/java/cn/my/chapter_1/queue/Josephus.java
+++ algorithm4/src/test/java/cn/my/chapter_1/queue/Josephus.java
@@ -2,8 +2,10 @@ package cn.my.chapter_1.queue;
 
 import java.util.ListIterator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * 约瑟夫环的问题
@@ -17,22 +19,56 @@ public class Josephus {
 	// 出队元素
 	private MyQueue<Integer> outQueue;
 
-	private int m;
+	private int k;
 
 	@BeforeEach
 	void init() {
-		int n = 7;
-		m = 2;
-		// 入队操作
 		myQueue = new MyQueue<>();
+		outQueue = new MyQueue<>();
+		k = 1;
+	}
+
+	@Test
+	@DisplayName("n=7,m=2,[1,3,5,0,4,2,6]")
+	void test1() {
+		int n = 7;
+		int m = 2;
+		String a = "1,3,5,0,4,2,6";
+		Assertions.assertEquals(a, test(n, m));
+	}
+
+	@Test
+	@DisplayName("n=3,m=5,[1,2,0]")
+	void test2() {
+		int n = 3;
+		int m = 5;
+		String a = "1,2,0";
+		Assertions.assertEquals(a, test(n, m));
+	}
+
+	@Test
+	@DisplayName("n=2,m=10,[1,0]")
+	void test3() {
+		int n = 2;
+		int m = 10;
+		String a = "1,0";
+		Assertions.assertEquals(a, test(n, m));
+	}
+
+	@Test
+	@DisplayName("n=2,m=11,[0,1]")
+	void test4() {
+		int n = 2;
+		int m = 11;
+		String a = "0,1";
+		Assertions.assertEquals(a, test(n, m));
+	}
+
+	private String test(int n, int m) {
 		for (int i = 0; i < n; i++) {
 			myQueue.enqueue(i);
 		}
-		outQueue = new MyQueue<>();
-	}
-
-	private String test() {
-		if (myQueue.isEmpty()) {
+		if (myQueue.isEmpty() || m < 1) {
 			throw new IllegalArgumentException();
 		}
 		if (myQueue.size() == 1) {
@@ -51,15 +87,30 @@ public class Josephus {
 					}
 					i = i + 1;
 				}
+				outQueue.enqueue(myQueue.dequeue());
 			} else {
+				outQueue.enqueue(myQueue.dequeue());
 				outQueue.enqueue(myQueue.dequeue());
 			}
 			return outQueue.toString();
 		} else {
-			while (myQueue.size() > 1) {
-				for (int i = 0; i < m - 1; i++) {
-					myQueue.add(myQueue.dequeue());
-				}
+			while (!myQueue.isEmpty()) {
+				dequeue(m, outQueue);
+			}
+		}
+		return outQueue.toString();
+	}
+
+	private void dequeue(int m, MyQueue<Integer> outQueue) {
+		ListIterator<Integer> iterator = myQueue.iterator();
+		while (iterator.hasNext()) {
+			int a = iterator.next();
+			if (k == m) {
+				outQueue.enqueue(a);
+				iterator.remove();
+				k = 1;
+			} else {
+				k = k + 1;
 			}
 		}
 	}
