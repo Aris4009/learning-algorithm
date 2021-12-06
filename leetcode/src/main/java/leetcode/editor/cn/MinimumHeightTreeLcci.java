e77b44f7ce908e7155c762bd77ddd3c1cc2e0570
aris <slut@vip.qq.com>
2021-12-06 11:26:39 +0800

    最小高度树

---
+++ leetcode/src/main/java/leetcode/editor/cn/MinimumHeightTreeLcci.java
@@ -0,0 +1,41 @@
+//给定一个有序整数数组，元素各不相同且按升序排列，编写一个算法，创建一棵高度最小的二叉搜索树。示例: 给定有序数组: [-10,-3,0,5,9], 一个可能
+//的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：           0          / \        -3 
+//  9        /   /      -10  5 Related Topics 树 二叉搜索树 数组 分治 二叉树 👍 106 👎 0
+
+package leetcode.editor.cn;
+
+import leetcode.editor.cn.my.TreeNode;
+
+public class MinimumHeightTreeLcci{
+    public static void main(String[] args){
+        Solution solution = new MinimumHeightTreeLcci().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length-1);
+    }
+
+    private TreeNode helper(int[] nums,int left,int right){
+        if (left>right){
+            return null;
+        }
+        int mid = left+((right-left)>>1);
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums,left,mid-1);
+        node.right = helper(nums,mid+1,right);
+        return node;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
