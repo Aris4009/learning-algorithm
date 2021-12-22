b1c50ec49c67e22d643947cde9008b507334924b
he <slut@vip.qq.com>
2021-12-22 16:07:31 +0800

    449

---
+++ leetcode/src/main/java/leetcode/editor/cn/SerializeAndDeserializeBst.java
@@ -0,0 +1,98 @@
+//序列化是将数据结构或对象转换为一系列位的过程，以便它可以存储在文件或内存缓冲区中，或通过网络连接链路传输，以便稍后在同一个或另一个计算机环境中重建。 
+//
+// 设计一个算法来序列化和反序列化 二叉搜索树 。 对序列化/反序列化算法的工作方式没有限制。 您只需确保二叉搜索树可以序列化为字符串，并且可以将该字符串反序
+//列化为最初的二叉搜索树。 
+//
+// 编码的字符串应尽可能紧凑。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [2,1,3]
+//输出：[2,1,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数范围是 [0, 10⁴] 
+// 0 <= Node.val <= 10⁴ 
+// 题目数据 保证 输入的树是一棵二叉搜索树。 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 设计 二叉搜索树 字符串 二叉树 👍 236 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import leetcode.editor.cn.my.TreeNode;
+
+public class SerializeAndDeserializeBst {
+	public static void main(String[] args) {
+//        Solution solution = new SerializeAndDeserializeBst().new Solution();
+//		Codec codec = new Codec();
+//		codec.deserialize("2,1,3");
+	}
+
+	// leetcode submit region begin(Prohibit modification and deletion)
+
+	/**
+	 * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+	 * left; TreeNode right; TreeNode(int x) { val = x; } }
+	 */
+	public class Codec {
+
+		// Encodes a tree to a single string.
+		public String serialize(TreeNode root) {
+			if (root == null) {
+				return "-1";
+			}
+			return root.val + "," + serialize(root.left) + "," + serialize(root.right);
+		}
+
+		// Decodes your encoded data to tree.
+		public TreeNode deserialize(String data) {
+			if (data == null || data.length() == 0) {
+				return null;
+			}
+			Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+			return helper(queue);
+		}
+
+		private TreeNode helper(Queue<String> queue) {
+			if (queue.isEmpty()) {
+				return null;
+			}
+			String k = queue.poll();
+			if (k.equals("-1")) {
+				return null;
+			}
+			TreeNode node = new TreeNode(Integer.parseInt(k));
+			node.left = helper(queue);
+			node.right = helper(queue);
+			return node;
+		}
+	}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
